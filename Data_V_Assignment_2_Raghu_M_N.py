c2949df8c4c96e3d097dc9a86263fb9c05cdf841
Raghumnstudent <68468698+Raghumnstudent@users.noreply.github.com>
2021-07-11 13:51:50 +0530

    Add files via upload
    Assignment_2

---
+++ Data_V_Assignment_2_Raghu_M_N.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+import pandas as pd
+import numpy as np
+
+na = pd.DataFrame(np.random.randint(0,10, size=(10,4)),columns=list('abcd'))
+na
+
+
+# In[ ]:
+
+
+
+
